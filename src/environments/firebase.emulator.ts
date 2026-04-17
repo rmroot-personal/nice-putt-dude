5d6a44684285d2437ca480bbf3c08c00a9351a3a
Robert Root <rmroot@live.com>
2026-04-17 18:58:32 -0500

    feat(WIP): initial firebase emulator setup

---
+++ src/environments/firebase.emulator.ts
@@ -0,0 +1,14 @@
+export const firebaseConfig = {
+    apiKey: "fake-api-key",
+    authDomain: "localhost",
+    projectId: "nice-putt-dude",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: "demo-app-id",
+    functionUrl: "http://localhost:5001/nice-putt-dude/us-central1"
+};
+
+// Set this to true for local development with emulators
+export const useEmulators = true;
+
+// To use production Firebase, set useEmulators to false and update firebaseConfig with your real credentials.
